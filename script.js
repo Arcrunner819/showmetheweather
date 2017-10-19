a9e70c3da2cca6de5d905734d21fe44b42a86f94
Arcrunner819 <jramosdirect2@gmail.com>
2017-10-19 14:13:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-  $.getJSON("http://ipinfo.io/json", function(data) {
+s$(document).ready(function() {
+  $.getJSON("https://ipinfo.io/json", function(data) {
     var ipdata = JSON.stringify(data);
     var obj = JSON.parse(ipdata);
 
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + obj.postal + ",us" + "&APPID=6170f94b82996e99d1820c54af6208f1", function(weather) {
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?zip=" + obj.postal + ",us" + "&APPID=6170f94b82996e99d1820c54af6208f1", function(weather) {
       var newWeather = JSON.stringify(weather)
       var obj2 = JSON.parse(newWeather)
       var temp = obj2.main.temp
@@ -16,7 +16,7 @@ $(document).ready(function() {
       var toMax = Math.floor(maxTemp * (9 / 5) - 459.67)
       var toMin = Math.floor(minTemp * (9 / 5) - 459.67)
 
-      document.getElementById("temperature").innerHTML = "<img width=120 src='http://openweathermap.org/img/w/" + obj2.weather[0].icon + ".png'></br>" + "Current Temperature: " + toFahr + "</br>" + "High for Today: " + toMax + "°F </br>" + "Low for Today: " + toMin + "°F"
+      document.getElementById("temperature").innerHTML = "<img width=120 src='https://openweathermap.org/img/w/" + obj2.weather[0].icon + ".png'></br>" + "Current Temperature: " + toFahr + "</br>" + "High for Today: " + toMax + "°F </br>" + "Low for Today: " + toMin + "°F"
       console.log(obj2)
 
       document.getElementById("location").innerHTML = "Location: " + obj2.name + "</br>" + "Humidity: " + obj2.main.humidity +"%"
